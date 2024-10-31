b35085a88aa0a72dee058ed3410b7af2bb75d019
kmw2378 <rlarla677@gmail.com>
2024-10-31 19:02:56 +0900

    [FEAT] CookieManager

---
+++ src/http_1_1_app/CookieManager.java
@@ -0,0 +1,16 @@
+package http_1_1_app;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CookieManager {
+    private final Map<String, String> cookies = new HashMap<>();
+
+    public void addCookie(String domain, String cookie) {
+        cookies.put(domain, cookie);
+    }
+
+    public String getCookies(String domain) {
+        return cookies.getOrDefault(domain, "");
+    }
+}
