d6ebcb9f9f8563b50a3f7b73c7f493ca3aeadb11
kmw2378 <rlarla677@gmail.com>
2024-10-31 19:07:03 +0900

    [FEAT] Main

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            CookieManager cookieManager = new CookieManager();
+
+            while (true) {
+                System.out.print("URL을 입력하세요 (종료하려면 'exit'): ");
+                String urlString = scanner.nextLine();
+                if (urlString.equalsIgnoreCase("exit")) {
+                    break;
+                }
+
+                // URL 파싱
+                URLParser urlParser = new URLParser(urlString);
+                System.out.println("URL 파싱 완료: " + urlParser.getHost() + ", 경로: " + urlParser.getPath());
+
+                // HttpClient로 요청 전송 및 응답 렌더링
+                HttpClient client = new HttpClient(urlParser.getHost(), urlParser.getPort(), urlParser.getPath(), cookieManager);
+                client.sendRequest();
+            }
+        } catch (Exception e) {
+            System.err.println("오류 발생: " + e.getMessage());
+        }
+    }
+}
